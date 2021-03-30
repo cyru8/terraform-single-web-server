2f4e685bea192ada24cdda79b3ec6b171a9c4cda
Cyru8 <olumide.adetiba@live.com>
2021-03-29 21:34:20 -0700

    first commit

---
+++ main.tf
@@ -0,0 +1,60 @@
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# DEPLOY A SINGLE EC2 INSTANCE
+# This template runs a simple "Hello, World" web server on a single EC2 Instance
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# ----------------------------------------------------------------------------------------------------------------------
+# REQUIRE A SPECIFIC TERRAFORM VERSION OR HIGHER
+# ----------------------------------------------------------------------------------------------------------------------
+
+terraform {
+  # This module is now only being tested with Terraform 0.13.x. However, to make upgrading easier, we are setting
+  # 0.12.26 as the minimum version, as that version added support for required_providers with source URLs, making it
+  # forwards compatible with 0.13.x code.
+  required_version = ">= 0.12.26"
+}
+
+# ------------------------------------------------------------------------------
+# CONFIGURE YOUR AWS CONNECTION
+# ------------------------------------------------------------------------------
+
+provider "aws" {
+  region = "us-east-2"
+}
+
+# ---------------------------------------------------------------------------------------------------------------------
+# DEPLOY A SINGLE EC2 INSTANCE
+# ---------------------------------------------------------------------------------------------------------------------
+
+resource "aws_instance" "example" {
+  # Ubuntu Server 20.04 LTS (HVM), SSD Volume Type in us-east-2
+  ami                    = "ami-08962a4068733a2b6"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, Cyru8!!!" > index.html
+              nohup busybox httpd -f -p "${var.server_port}" &
+              EOF
+
+  tags = {
+    Name = "terraform-example"
+  }
+}
+
+# ---------------------------------------------------------------------------------------------------------------------
+# CREATE THE SECURITY GROUP THAT'S APPLIED TO THE EC2 INSTANCE
+# ---------------------------------------------------------------------------------------------------------------------
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  # Inbound HTTP from anywhere
+  ingress {
+    from_port   = var.server_port
+    to_port     = var.server_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
